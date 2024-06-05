5598d19145fca4045f5cd7f2f515f8b94cf639bb
Andre <andre_dean@me.com>
2024-06-05 12:54:48 -0700

    proxy modifications

--- client/streaming-statistics/src/setupProxy.js
+++ client/streaming-statistics/src/setupProxy.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
     createProxyMiddleware(["/api"], {
-      target: "https://streaming-analytics.onrender.com/api/upload/csv",
+      target: "https://streaming-analytics.onrender.com/api/upload/csv/netflix",
       changeOrigin: true,
     })
   );
