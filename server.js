0dcc5509c9d0a9d5b1836a30dd46f5d7d61d1265
Andre <andre_dean@me.com>
2024-06-09 12:12:11 -0700

    ensure tmp/uploads is created

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const fs = require("fs");
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-if (!fs.existsSync("./tmp/upload")) {
-  fs.mkdirSync("./tmp/upload", { recursive: true });
+if (!fs.existsSync("./tmp/uploads")) {
+  fs.mkdirSync("./tmp/uploads", { recursive: true });
 }
 app.use(cors());
 app.use(express.json({ extended: false }));
